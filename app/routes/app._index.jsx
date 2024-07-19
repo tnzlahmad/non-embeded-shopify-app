828827117bc5c8bab2dd246f6ea18d4a61cc546a
Ibrahim Alvi <ibrahim@techibits.com>
2024-07-19 19:32:48 +0500

    Store data in db and fetch also , Sync data from shopify

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,81 +1,145 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 import { useEffect, useState } from "react";
 import { json } from "@remix-run/node";
-import { useFetcher, useLoaderData } from "@remix-run/react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  Page,
-  Button,
-  IndexTable,
-  ButtonGroup
-} from "@shopify/polaris";
+import { useFetcher, useLoaderData , useSubmit } from "@remix-run/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Page, Button, IndexTable, ButtonGroup } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
-import { CustomDataTable, CustomCard, ProductCatalog, FAQsComponent, Header } from "./components";
+import {saveProducts} from "../utitls/product.server";
+import {
+  CustomDataTable,
+  CustomCard,
+  ProductCatalog,
+  FAQsComponent,
+  Header,
+} from "./components";
+import prisma from "../db.server";
 
-// Loader function to get the shop name
 export async function loader({ request }) {
   const { admin, session } = await authenticate.admin(request);
   const data = await admin.rest.resources.Product.all({ session });
   const shopName = session.shop;
-  console.log('data', data);
-  return json({ shopName, data });
+  const productFieldsData = await prisma.productFields.findMany();
+  return json({ shopName, productFieldsData, data });
+}
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const actionType = formData.get("actionType");
+
+  if (actionType === "syncProducts") {
+    return await syncProducts(request);
+  } else {
+    const id = formData.get("id");
+    try {
+      await prisma.productFields.delete({
+        where: { id: Number(id) },
+      });
+      return redirect("/products");
+    } catch (error) {
+      return json({ error: "Failed to delete product" }, { status: 500 });
+    }
+  }
+};
+
+async function syncProducts(request) {
+  const { admin, session } = await authenticate.admin(request);
+  const data = await admin.rest.resources.Product.all({ session });
+  await saveProducts(data.data);
+  return json({ message: "Products synced successfully" });
 }
 
 export default function Index() {
   const itemsPerPage = 5;
-  const { shopName, data } = useLoaderData();
+  const { shopName, productFieldsData } = useLoaderData();
   const [currentPage, setCurrentPage] = useState(1);
-
-  // Calculate current items to display based on pagination
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const currentItems = data.data.slice(startIndex, endIndex);
+  const currentItems = productFieldsData.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(productFieldsData.length / itemsPerPage);
 
-  // Calculate total pages for pagination
-  const totalPages = Math.ceil(data.data.length / itemsPerPage);
-
-  // Handle page change
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
 
+  const submit = useSubmit();
+  const handleSync = () => {
+    const formData = new FormData();
+    formData.append("actionType", "syncProducts");
+    submit(formData, { method: "post" });
+  };
+
+  const fetcher = useFetcher();
+  const handleDelete = async (id) => {
+    const formData = new FormData();
+    formData.append("id", id);
+    fetcher.submit(formData, { method: "post" });
+  };
+
   // Generate rows for IndexTable
-  const rowMarkup = currentItems.map(({ image, id, title }, index) => (
+  const rowMarkup = currentItems.map(({ image, id, feedName }, index) => (
     <IndexTable.Row id={id} key={id} position={index}>
-      <IndexTable.Cell><img height={"40px"} src={image.src} alt={title} /></IndexTable.Cell>
+      <IndexTable.Cell>
+        {image ? (
+          <img height={"40px"} src={image.src} alt={title} />
+        ) : (
+          <span>No Image</span>
+        )}
+      </IndexTable.Cell>
       <IndexTable.Cell>{id}</IndexTable.Cell>
-      <IndexTable.Cell>{title}</IndexTable.Cell>
+      <IndexTable.Cell>{feedName}</IndexTable.Cell>
+      <IndexTable.Cell>
+        <ButtonGroup>
+          <Button>Edit</Button>
+          <Button type="button" onClick={() => handleDelete(id)}>
+            Delete
+          </Button>
+        </ButtonGroup>
+      </IndexTable.Cell>
     </IndexTable.Row>
   ));
 
   return (
     <Page>
       <Header />
+
+      <Button onClick={handleSync}>Sync</Button>
+
       <IndexTable
-        itemCount={data.data.length} // Corrected itemCount to reflect the correct length
+        itemCount={productFieldsData.length}
         headings={[
-          { title: 'Image' },
-          { title: 'Id' },
-          { title: 'Title' },
+          { title: "Image" },
+          { title: "Id" },
+          { title: "Title" },
+          { title: "Action" },
         ]}
         selectable={false}
       >
         {rowMarkup}
       </IndexTable>
 
-      <div className='polaris-btn'>
+      <div className="polaris-btn mt-3">
         <ButtonGroup segmented>
-          <Button primary onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+          <Button
+            primary
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
             Previous Page
           </Button>
-          <span className='space-page'>{currentPage} / {totalPages}</span>
-          <Button primary onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+          <span className="space-page">
+            {currentPage} / {totalPages}
+          </span>
+          <Button
+            primary
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
             Next Page
           </Button>
         </ButtonGroup>
       </div>
-
       <ProductCatalog />
       <FAQsComponent />
     </Page>
--- app/routes/app.productFeed.jsx
+++ app/routes/app.productFeed.jsx
@@ -1,83 +1,127 @@
 import React from "react";
-import { Page, Card , Text } from "@shopify/polaris";
-import { Row, Col } from "reactstrap";
+import shopify from "../shopify.server";
+import { json } from "@remix-run/node";
+import { useLoaderData, useFetcher } from "@remix-run/react";
+import { Page, Card, Text, Button } from "@shopify/polaris";
 import { Header, TextFields, CheckBox, SelectBox } from "./components";
+import { Row, Col } from "reactstrap";
+import prisma from "../db.server";
+
+export async function loader({ request }) {
+  const { admin, session } = await shopify.authenticate.admin(request);
+  const data = await admin.rest.resources.Product.all({ session });
+  return json(data);
+}
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const feedName = formData.get("feedName");
+  try {
+    const existingProduct = await prisma.productFields.create({
+      data: {
+        feedName,
+      },
+    });
+
+    return json({ success: true, product: existingProduct });
+  } catch (error) {
+    console.error("Error saving product feed name:", error);
+    return json({ success: false, error: error.message });
+  }
+};
 
 export default function ChangePlan() {
+  const fetcher = useFetcher();
+
+  const handleSaveClick = (event) => {
+    event.preventDefault();
+    const form = document.getElementById("productFeedForm");
+    if (form instanceof HTMLFormElement) {
+      fetcher.submit(new FormData(form), { method: "post" });
+    } else {
+      console.error("Form element not found or not of type HTMLFormElement");
+    }
+  };
+
   return (
     <Page>
       <Header />
       <div className="mt-4">
         <Card>
-          <Row>
-            {/* Left Section */}
-            <Col md="9">
-              {/* TextField */}
-              <div className="row">
-              <Text as="h4" variant="headingLg">Add a product feed</Text>
-                <TextFields
-                  placeholder="Enter Product Feed Name"
-                  type="text"
-                  name=""
-                  classColMd="col-md-6"
-                />
-                <TextFields
-                  placeholder="Enter Currency (PKR Only)"
-                  type="text"
-                  name=""
-                  classColMd="col-md-6"
-                />
-              </div>
+          <form id="productFeedForm">
+            <Row>
+              {/* Left Section */}
+              <Col md="9">
+                {/* TextField */}
+                <div className="row">
+                  <Text as="h4" variant="headingLg">
+                    Add a product feed
+                  </Text>
+                  <TextFields
+                    placeholder="Enter Product Feed Name"
+                    type="text"
+                    name="feedName"
+                    classColMd="col-md-6"
+                  />
+                  <TextFields
+                    placeholder="Enter Currency (PKR Only)"
+                    type="text"
+                    name=""
+                    classColMd="col-md-6"
+                  />
+                </div>
 
-              {/* CheckBox */}
-              <div className="mt-3">
-              <h5>Append currency parameter to product URL?</h5>
-                <CheckBox
-                  labelOne="Do Not append. (default, example: my-store.com/products/my-product)"
-                  labelTwo="Do append. (example: my-store.com/products/my-product?currency=USD)"
-                  name="CheckBox1"
-                />
-              </div>
-              <div className="mt-3">
-              <h5>All products or some of them?</h5>
-                <CheckBox
-                  labelOne="All products"
-                  labelTwo="Products from selected collection"
-                  name="CheckBox2"
-                />
-              </div>
-              {/* Text Box */}
-              <div className="row">
-                <h5>Another Text Box</h5>
-                <TextFields
-                  placeholder="Text Field"
-                  type="text"
-                  name=""
-                  classColMd="col-md-6"
-                />
-                <TextFields
-                  placeholder="Text Field"
-                  type="text"
-                  name=""
-                  classColMd="col-md-6"
-                />
-              </div>
+                {/* CheckBox */}
+                <div className="mt-3">
+                  <h5>Append currency parameter to product URL?</h5>
+                  <CheckBox
+                    labelOne="Do Not append. (default, example: my-store.com/products/my-product)"
+                    labelTwo="Do append. (example: my-store.com/products/my-product?currency=USD)"
+                    name="CheckBox1"
+                  />
+                </div>
+                <div className="mt-3">
+                  <h5>All products or some of them?</h5>
+                  <CheckBox
+                    labelOne="All products"
+                    labelTwo="Products from selected collection"
+                    name="CheckBox2"
+                  />
+                </div>
+                {/* Text Box */}
+                <div className="row">
+                  <h5>Another Text Box</h5>
+                  <TextFields
+                    placeholder="Text Field"
+                    type="text"
+                    name=""
+                    classColMd="col-md-6"
+                  />
+                  <TextFields
+                    placeholder="Text Field"
+                    type="text"
+                    name=""
+                    classColMd="col-md-6"
+                  />
+                </div>
 
-              {/* Select Box */}
-              <div className="row mt-3">
-              <h5>Select Box</h5>
-              <SelectBox classColMd="col-md-12" />
-              </div>
-            </Col>
+                {/* Select Box */}
+                <div className="row mt-3">
+                  <h5>Select Box</h5>
+                  <SelectBox classColMd="col-md-12" />
+                </div>
+              </Col>
 
-            {/* Right Section */}
-            <Col md="3">
-              <div>
-                <p>This is your right section content.</p>
-                <p>This is your right section content.</p>
-              </div>
-            </Col>
-          </Row>
+              {/* Right Section */}
+              <Col md="3">
+                <div>
+                  <p>This is your right section content.</p>
+                  <p>This is your right section content.</p>
+                </div>
+              </Col>
+            </Row>
+            <button type="button" onClick={handleSaveClick} className="btn btn-primary mt-3">Save</button>
+          </form>
         </Card>
       </div>
     </Page>
