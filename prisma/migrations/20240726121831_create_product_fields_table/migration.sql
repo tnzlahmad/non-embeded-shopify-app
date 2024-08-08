c8268005399bea234cec087ba66fcc2a7f6cfaea
Muhammad Ibrahim Alvi <ibrahim@techibits.com>
2024-08-08 16:25:08 +0500

    Migration Changes + Pkg.json

--- prisma/migrations/20240726121831_create_product_fields_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "ProductFields" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "feedName" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
